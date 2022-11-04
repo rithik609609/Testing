027ca5ae6f82a406cc87542ad13289233fc0071a
Rithik Prakash <rithik.prakash@sap.com>
2022-11-04 11:46:00 +0530

    File Edit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Car myFerrari = new Car("Ferrari","400"); 
+        myFerrari.moveForward();
+        myFerrari.brake();
+    }
+}
